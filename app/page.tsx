6a02e3cde5882d7e2c9fb68bb81b18222b23ba12
samuelkime267 <samuelkime7@gmail.com>
2025-01-02 04:42:00 +0100

    added count

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,24 @@ export default function Home() {
   const activeIndex = Math.floor(sections.length / 2);
   return (
     <main>
-      <section className="fixed top-[81.3%] left-[18.75%] p-2">
+      <section className="fixed top-[81.3%] left-[18.75%] p-2 flex items-start justify-start gap-4">
+        <div className="relative">
+          {sections.map((_, i) => {
+            const isActive = i === activeIndex;
+            return (
+              <div key={i} className="absolute top-0 left-0 overflow-hidden">
+                <p
+                  className={cn(
+                    "font-light text-xs text-neutral-600",
+                    !isActive && "translate-y-full"
+                  )}
+                >
+                  0{i + 1}
+                </p>
+              </div>
+            );
+          })}
+        </div>
         <div className="relative">
           {sections.map((section, i) => {
             const isActive = i === activeIndex;
