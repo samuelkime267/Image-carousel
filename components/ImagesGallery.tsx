c29368d6cb14dac2790f1cc09a44df36159a92f9
samuelkime267 <samuelkime7@gmail.com>
2025-01-06 22:22:02 +0100

    rearranged images position

--- components/ImagesGallery.tsx
+++ components/ImagesGallery.tsx
@@ -12,7 +12,7 @@ export default function ImagesGallery({ images, name }: ImagesGalleryProps) {
     <section className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4">
       <div className="grid grid-cols-1 gap-4 h-fit">
         {images.slice(1).map((image, i) => {
-          if (i % 2 === 0) return;
+          if (i % 2 !== 0) return;
           return (
             <div key={i} className="bg-gray-400">
               <img src={image} alt={name} className="w-full h-auto" />
@@ -22,7 +22,7 @@ export default function ImagesGallery({ images, name }: ImagesGalleryProps) {
       </div>
       <div className="grid grid-cols-1 gap-4 h-fit">
         {images.slice(1).map((image, i) => {
-          if (i % 2 !== 0) return;
+          if (i % 2 === 0) return;
           return (
             <div key={i} className="bg-gray-400">
               <img src={image} alt={name} className="w-full h-auto" />
